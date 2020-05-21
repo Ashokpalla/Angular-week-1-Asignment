167e725f7859562ff9ead6256702aab0f176a856
Palla Ashok <ashokpalla488@gmail.com>
2020-05-21 20:15:33 +0530

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,28 @@ import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 @NgModule({
 	declarations: [
 		AppComponent,
 		MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   
 	],
 	imports: [
@@ -34,6 +41,7 @@ import { FooterComponent } from './footer/footer.component';
     MatButtonModule,
     MatCardModule,
     MatListModule,
+    AppRoutingModule,
 		FlexLayoutModule
 	],
 	providers: [
